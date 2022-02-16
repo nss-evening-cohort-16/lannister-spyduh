dafb7997f898ede76e0ec8e60ac5f169162ab0ab
pmpurcell93 <maddenpurcell@gmail.com>
2022-02-15 20:27:27 -0600

    Added Spy Repository file

---
+++ Lannister Spyduh/DataAccess/SpyRepository.cs
@@ -0,0 +1,54 @@
+﻿using Lannister_Spyduh.Models;
+using System.Collections.Generic;
+
+namespace Lannister_Spyduh.DataAccess
+{
+    public class SpyRepository
+    {
+        private static List<Spy> _spies = new List<Spy>()
+        {
+            new Spy()
+            {
+                CodeName = "GooseGoon Purcell",
+                faction = Faction.Blue,
+                Id = 1,
+                skillType = SkillType.Espionage,
+                service = Service.Babysitting,
+            },
+            new Spy()
+            {
+                CodeName = "Nissa Night",
+                faction = Faction.Blue,
+                Id = 2,
+                skillType = SkillType.MartialArts,
+                service = Service.Bodyguard,
+            },
+            new Spy()
+            {
+                CodeName = "Moonlight Baylor",
+                faction = Faction.Blue,
+                Id = 3,
+                skillType = SkillType.Tracking,
+                service = Service.Weddings,
+            },
+            new Spy()
+            {
+                CodeName = "Jigsawret",
+                faction = Faction.Red,
+                Id = 4,
+                skillType = SkillType.Demolitions,
+                service = Service.Babysitting,
+            },
+            new Spy()
+            {
+                CodeName = "Bristopher",
+                faction = Faction.Red,
+                Id = 5,
+                skillType = SkillType.Hacking,
+                service = Service.ArtHeist,
+            }
+        };
+
+    }
+}
+
